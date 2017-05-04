b5811d7446a5e60facb7da0578168fc60549ceb3
qinyi <qinyi>
2017-05-03 22:21:01 -0700

    conv2

--- conv_2.v
+++ conv_2.v
@@ -24,6 +24,7 @@ module conv_2(
 input clk,
 input rst,
 input conv_2_en,
+input [100*17-1:0] wr_data, 
 output reg bias_bram_en,
 output reg [6 : 0] bias_bram_addr,
 output reg fm_bram_ena,//read
@@ -32,6 +33,13 @@ output reg [4 : 0] fm_bram_addra,
 output reg [4 : 0] fm_bram_addrb,
 output reg conv_w_bram_en,
 output reg [11 : 0] conv_w_bram_addr,
+output reg fm_bram_1_wea,
+output reg fm_bram_1_web,
+output reg [6:0] fm_bram_1_addra,
+output reg [6:0] fm_bram_1_addrb,
+output reg [56 * 16-1 : 0] fm_bram_1_dina,
+output reg [56 * 16-1 : 0] fm_bram_1_dinb,
+output reg store_en,
 output conv_2_finish
     );
     
@@ -40,6 +48,9 @@ reg [3 : 0] fm_out;
 reg finish;
 reg conv_2_en_d;
 wire conv_2_en_p;
+reg [3:0] store_en_pre;
+reg [1:0] store_en_d;
+
 always @ (posedge clk) begin
     conv_2_en_d <= conv_2_en;
 end
@@ -71,6 +82,22 @@ begin
         fm_out <= 0;
     else if (conv_w_cnt == 300)
         fm_out <= fm_out + 1;
+end
+
+always @ (posedge clk)
+begin
+    store_en_pre[0] <= (conv_w_cnt == 300);
+    store_en_pre[3:1] <= store_en_pre[2:0];
+end
+
+always @ (posedge clk)
+begin
+    store_en <= store_en_pre[3] || store_en_pre[2];
+end
+
+always @ (posedge clk)
+begin
+    store_en_d <= {store_en_d[0],store_en};
 end
 
 always @ (posedge clk)
@@ -133,6 +160,8 @@ always @ (posedge clk)
 begin
     if (conv_2_en_p)
         bias_bram_en <= 1;
+    else if (conv_2_en && store_en_pre[0])
+        bias_bram_en <= 1;
     else bias_bram_en <= 0;
 end
 
@@ -140,6 +169,58 @@ always @ (posedge clk)
 begin
     if (conv_2_en_p)
         bias_bram_addr <= 4;
+    else if (bias_bram_en)
+        bias_bram_addr <= bias_bram_addr + 1;
+end
+
+//fm_bram_1_wea
+always @ (posedge clk) begin
+    if (rst)
+        fm_bram_1_wea <= 0;        
+    else 
+        fm_bram_1_wea <= store_en_d[0];
+end
+//fm_bram_1_addra
+always @ (posedge clk) begin
+    if (conv_2_en_p)
+        fm_bram_1_addra <= 0;
+    else if (store_en_d[1] && store_en_d[0])
+        fm_bram_1_addra <= fm_bram_1_addra + 16;
+    else if (~store_en_d[0] && store_en_d[1])
+        fm_bram_1_addra <= fm_bram_1_addra - 15;
+end
+integer i;
+//fm_bram_1_dina
+always @ (posedge clk) begin
+    fm_bram_1_dina[56*16-1 : 50*16] <= 0;
+    if (store_en_d[0]) 
+    for (i = 0; i < 50 ;i = i + 1)
+        fm_bram_1_dina[i*16 +: 16]  <= wr_data[17*56+i*17+1 +: 16] + wr_data[17*56+i*17] ;      
+end
+
+//fm_bram_1_wea
+always @ (posedge clk) begin
+    if (rst)
+        fm_bram_1_web <= 0;        
+    else 
+        fm_bram_1_web <= store_en_d[0];
+end
+//fm_bram_1_addra
+always @ (posedge clk) begin
+    if (conv_2_en_p)
+        fm_bram_1_addrb <= 1;
+    else if (store_en_d[1] && store_en_d[0])
+        fm_bram_1_addrb <= fm_bram_1_addrb + 16;
+    else if (~store_en_d[0] && store_en_d[1])
+        fm_bram_1_addrb <= fm_bram_1_addrb - 15;
+end
+integer i;
+//fm_bram_1_dina
+always @ (posedge clk) begin
+    fm_bram_1_dinb[56*16-1 : 50*16] <= 0;
+    if (store_en_d[0]) 
+    for (i = 0; i < 50 ;i = i + 1)
+        fm_bram_1_dinb[i*16 +: 16]  <= wr_data[17*56+i*17+1 +: 16] + wr_data[17*56+i*17] ;      
 end
 
 endmodule
